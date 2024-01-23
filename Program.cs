f030fba10490c26b6501a32d77c7ccc28612c40f
unknown <namitlodh>
2024-01-23 16:18:04 +0530

    Qn9: Sum of each rows in a matirx

--- Program.cs
+++ Program.cs
@@ -1,6 +1,34 @@
-﻿using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;namespace Arrays_problems{    internal class Program    {        static void Main(string[] args)        {            int number = int.Parse(Console.ReadLine());            int sum = 0;            int m;            while (number > 0)
+﻿using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;namespace Arrays_problems{    internal class Program    {        static void Main(string[] args)        {
+            Console.WriteLine("Enter rows:");
+            int row = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter columns:");
+            int col = Convert.ToInt32(Console.ReadLine()); 
+            int[,] Matrix = new int[row, col];
+            Console.Write("Enter the elements of matrix: ");
+            for (int i = 0; i < row; i++)
             {
-                m= number%10;
-                sum += m;
-                number = number/10;
-            }            Console.WriteLine($"Sum is = {sum}");            Console.ReadLine();        }    }}
+                for (int j = 0; j < col; j++)
+                {
+                    Matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("\nMatrix: ");
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write(Matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < row; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < col; j++)
+                {
+                    sum += Matrix[i, j];
+                }
+                Console.WriteLine($"Sum of row [{i + 1}] is {sum}");
+            }
+            Console.ReadLine();
+        }    }}

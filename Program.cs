c2c954bbfdbf823bd68bbedcb5476f2614bb91d0
unknown <namitlodh>
2024-01-23 11:09:22 +0530

    Qn1: Count Duplicate elements

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_problems
+{
+    internal class Program
+    {
+        static void CountOccurrences(int[] arr, int len)
+        {
+            Dictionary<int, int> elementCount = new Dictionary<int, int>();
+
+            for (int i = 0; i < len; i++)
+            {
+                if (elementCount.ContainsKey(arr[i]))
+                {
+                    elementCount[arr[i]]++;
+                }
+                else
+                {
+                    elementCount.Add(arr[i], 1);
+                }
+            }
+            foreach (var entry in elementCount)
+            {
+                Console.WriteLine($"{entry.Key} occurs {entry.Value} times");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 1,1,2,1,2 };
+            int n = arr.Length;
+            CountOccurrences(arr, n);
+            Console.ReadLine();
+        }
+    }
+}

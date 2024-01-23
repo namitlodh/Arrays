000bdaf5dca012fc8c6101ba5ff18586b7264ad2
unknown <namitlodh>
2024-01-23 11:32:49 +0530

    Qn4: max and min element

--- Program.cs
+++ Program.cs
@@ -8,35 +8,22 @@ namespace Arrays_problems
 {
     internal class Program
     {
-        static void CountOccurrences(int[] arr, int len)
-        {
-            Dictionary<int, int> elementCount = new Dictionary<int, int>();
-
-            for (int i = 0; i < len; i++)
-            {
-                if (elementCount.ContainsKey(arr[i]))
-                {
-                    elementCount[arr[i]]++;
-                }
-                else
-                {
-                    elementCount.Add(arr[i], 1);
-                }
-
-
-            }
-            foreach (var entry in elementCount)
-            {
-                if(entry.Value > 1)
-                Console.WriteLine($"{entry.Key}");
-            }
-        }
-
         static void Main(string[] args)
         {
-            int[] arr = { 1,1,2,1,2,3,3};
-            int n = arr.Length;
-            CountOccurrences(arr, n);
+            int[] arr = { 1,1,2,1,2,3,3,20, 10, -1};
+            int len = arr.Length;
+            int max = arr[0]; 
+            int min = arr[0] ;
+            for (int i = 0; i < len; i++) 
+            {
+                if (arr[i] >max)
+                    max = arr[i];
+                else if (arr[i] <min)
+                    min = arr[i];
+            }
+            Console.WriteLine($"maximun element is: {max}");
+            Console.WriteLine($"minimum element is: {min}");
+
             Console.ReadLine();
         }
     }

29886a46916ac8b95cf14b888937d19b3eed7dc6
unknown <namitlodh>
2024-01-23 15:59:00 +0530

    Qn8: Sum of the digits in a number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;namespace Arrays_problems{    internal class Program    {        static void Main(string[] args)        {            string sentence = Console.ReadLine();            string[] words = sentence.Split(' ');
-            int len = words.Length;            for (int i = 0; i < len; i++)            {                char[] rev = words[i].ToCharArray();                Array.Reverse(rev);                words[i] = new string(rev);            }            string reversedString = string.Join(" ", words);            Console.WriteLine(reversedString);            Console.ReadLine();        }    }}
+﻿using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;namespace Arrays_problems{    internal class Program    {        static void Main(string[] args)        {            int number = int.Parse(Console.ReadLine());            int sum = 0;            int m;            while (number > 0)
+            {
+                m= number%10;
+                sum += m;
+                number = number/10;
+            }            Console.WriteLine($"Sum is = {sum}");            Console.ReadLine();        }    }}
